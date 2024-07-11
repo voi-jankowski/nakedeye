403b873dbd280b975be98b942e364ec23f70c788
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-11 16:22:02 +1000

    Set custom theme for Chakra, with no border radius on containers and boxes and gray 50 global backround.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
 import "./index.css";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#F7FAFC",
+      },
+    },
+  },
+  components: {
+    Box: {
+      baseStyle: {
+        borderRadius: "0",
+      },
+    },
+    Container: {
+      baseStyle: {
+        borderRadius: "0",
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
