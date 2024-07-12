e4047976a477efc1ad836bab3cb259b58ffa6331
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-12 13:53:56 +1000

    Removed heights of containers in main.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,21 +7,20 @@ import Projects from "./Projects";
 const Home = () => (
   <Container maxW="container.2xl" bg="rgb(255, 255, 255)" p={0}>
     <VStack
-      height="80vh"
       spacing={0}
       m={0}
       bg="url('https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68')"
       bgSize="cover"
       bgPosition="center"
     >
-      <Box height="75vh" width="100%" p={0} m={0}>
+      <Box height="65vh" width="100%" p={0} m={0}>
         <WelcomeHeading />
       </Box>
-      <Box height="25vh" width="100%" p={0} m={0}>
+      <Box width="100%" p={0} m={0}>
         <Benefits />
       </Box>
     </VStack>
-    <Box height="20vh" bg="transparent">
+    <Box bg="transparent">
       <Projects />
     </Box>
   </Container>
