f74c6a23270940d5bed5e4f72ecaa95696b9b8de
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-15 16:11:54 +1000

    Adjusted sizing and added logo to the Header, so it is reactive to displays.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,36 @@
 // src/components/Header.tsx
-import { Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Flex, HStack, Text, Image } from "@chakra-ui/react";
 
 const Header = () => (
   <Flex
     as="header"
     position="fixed"
-    height={32}
+    h={{ base: 20, sm: 20, md: 28, lg: 32, xl: 32, "2xl": 36 }}
     top="0"
     left="0"
     width="100%"
-    bg="offwhite"
-    p={2}
+    bg="black"
+    p={{ base: 6, sm: 6, md: 8, lg: 10, xl: 10, "2xl": 16 }}
     zIndex={1}
+    justifyContent="space-between"
+    alignItems="center"
   >
-    <Container
-      maxW="container.xl"
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Text color="blue" fontSize="2xl" fontWeight="bold">
-        nakedEyeCreative
-      </Text>
-      <HStack spacing={8} color="blue" fontWeight={"bold"} fontSize={"md"}>
-        <Text>Menu Item 1</Text>
-        <Text>Menu Item 2</Text>
-        <Text>Menu Item 3</Text>
-      </HStack>
-    </Container>
+    <Image
+      src="public/assets/Naked_Eye.png"
+      alt="Logo"
+      width={{
+        base: 60,
+        sm: 72,
+        md: 96,
+        lg: "28rem",
+        xl: "28rem",
+        "2xl": "30rem",
+      }}
+      pt={{ base: 10, sm: 12, md: 20, lg: 20, xl: 20, "2xl": 20 }}
+    />
+    <HStack spacing={8} color="blue" fontWeight={"bold"} fontSize={"md"}>
+      <Text>Menu Item 1</Text>?
+    </HStack>
   </Flex>
 );
 
